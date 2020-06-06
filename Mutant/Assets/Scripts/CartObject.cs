4fbfebfd126237e14510420d4b03a28890cf2936
Dima Shymko <trozeman@gmail.com>
2020-06-06 04:38:01 +0300

    First Worked commit.....

---
+++ Mutant/Assets/Scripts/CartObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartObject : MonoBehaviour
+{
+
+  [SerializeField] Sprite cartSprite;
+  [SerializeField] int dmg;
+  [SerializeField] int defence;
+  [SerializeField] int heal;
+  [SerializeField] SpriteRenderer iconRenderer;
+  [SerializeField] SpriteRenderer overlayRenderer;
+  [SerializeField] int curentOverlay = 0;
+  [SerializeField] List<Sprite> overlay;
+  [SerializeField] Sprite icon;
+
+
+  void Awake()
+  {
+      overlayRenderer.sprite = overlay[curentOverlay];
+      iconRenderer.sprite = icon;
+  }
+
+}
