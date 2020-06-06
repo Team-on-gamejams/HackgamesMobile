ea97e8f3a1b19770f82617627dc786262353b8ad
Team <timonsol13@gmail.com>
2020-06-06 05:21:05 +0300

    Inventory ui mockup

---
+++ Mutant/Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+	[SerializeField] Button[] buttons;
+	[SerializeField] RectTransform[] tabs;
+
+	int selectedId = 0;
+
+	private void Awake() {
+		buttons[selectedId].interactable = false;
+		for(int i = 0; i < tabs.Length; ++i)
+			tabs[i].gameObject.SetActive(i == selectedId);
+	}
+
+	public void OpenTab(int id) {
+		buttons[selectedId].interactable = true;
+		tabs[id].gameObject.SetActive(false);
+		selectedId = id;
+		buttons[selectedId].interactable = false;
+		tabs[id].gameObject.SetActive(true);
+	}
+}
